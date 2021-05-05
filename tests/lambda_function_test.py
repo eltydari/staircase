e7da8ec9724207c1df430a45edf8f81e596487d1
George Ge <2453869+eltydari@users.noreply.github.com>
2021-05-05 18:36:34 -0400

    Added automated testing

---
+++ tests/lambda_function_test.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+import lambdas.lambda_function as module
+
+class ModuleTests(TestCase):
+
+    @patch.object(module, "log_new_invocation")
+    def test_lambda_handler(self, invocation_mock):
+        response = module.lambda_handler(MagicMock(), MagicMock())
+        self.assertEqual(response, {
+            'statusCode': 200,
+            'body': "helloworld"  # MUST return helloworld!
+        })
+        self.assertEqual(1, invocation_mock.call_count)
+
+    @patch("builtins.open")
+    @patch("tempfile.mkstemp", return_value=("test", "test"))
+    @patch("boto3.resource")
+    def test_log_new_invocation(self, resource_mock, mkstemp_mock, _open_mock):
+        module.log_new_invocation()
+        s3_mock = resource_mock().Bucket()
+        self.assertEqual(1, s3_mock.download_fileobj.call_count)
+        self.assertEqual(1, s3_mock.upload_file.call_count)
+        self.assertEqual(1, mkstemp_mock.call_count)
