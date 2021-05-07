921c2a27b65185dbd4bb35151451619e17817816
George Ge <2453869+eltydari@users.noreply.github.com>
2021-05-07 04:20:39 -0400

    Add quicksight update procedure

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,12 +4,13 @@ from datetime import datetime
 import tempfile
 import os
 
-BUCKET_LOCATION = "staircase-demo"
+ACCOUNT_ID = "362764577362"
 INVOCATION_FILENAME = "logs/invocations.csv"
 
 
 def lambda_handler(event, context):
     log_new_invocation()
+    report_invocation()
     return "helloworld"
 
 
@@ -18,9 +19,18 @@ def log_new_invocation():
     ifile.log_invocation()
 
 
+def report_invocation():
+    quicksight = boto3.client('quicksight')
+    quicksight.create_ingestion(
+        DataSetId = "8b37060b-d5ea-4950-b5a0-22e9ed3d2cd3",
+        IngestionId = datetime.utcnow().isoformat(),
+        AwsAccountId = ACCOUNT_ID
+    )
+
+
 class InvocationFile(object):
     def __init__(self):
-        self._bucket = boto3.resource('s3').Bucket(BUCKET_LOCATION)
+        self._bucket = boto3.resource('s3').Bucket("staircase-demo")
         _, self._filename = tempfile.mkstemp()
         try:
             with open(self._filename, 'wb') as f:
